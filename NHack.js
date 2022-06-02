45fe65a8a6cde461d19ee308dfd001290de8f569
SlothScript <106759481+SlothScript@users.noreply.github.com>
2022-06-02 16:43:42 -0500

    Create NHack.js

---
+++ NHack.js
@@ -0,0 +1,8 @@
+if (typeof NHack !== 'undefined') {
+    if (NHack === null) {
+        delete NHack;
+    } else throw new Error('NHack already loaded.');
+}
+var NHack = {
+    Game.ClickCookie();
+}
